c96fba3f91a12caf42a162a54f4b2add4fa016d4
hands8142 <dongsu8142@naver.com>
2022-02-14 15:24:40 +0900

    feat: add leaderboard

---
+++ src/commands/Economy/leaderboard.ts
@@ -0,0 +1,39 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
+import DiscordClient from "../../client/client";
+import { CommandType } from "../../utils/types";
+
+export default {
+  data: new SlashCommandBuilder()
+    .setName("순위")
+    .setDescription("1위부터 10위까지의 순위를 보여줍니다."),
+  async execute(client, interaction, embed) {
+    const users = (await client.economyRepository.find())
+      .sort((a, b) => b.money - a.money)
+      .slice(0, 10);
+    interaction.reply({
+      embeds: [
+        embed
+          .setTitle("순위")
+          .setDescription(
+            users
+              .map(
+                async (user, index) =>
+                  `${index + 1}. ${await getUserName(client, user.userId)} - ${
+                    user.money
+                  }`
+              )
+              .join("\n")
+          ),
+      ],
+    });
+  },
+} as CommandType;
+
+const getUserName = async (client: DiscordClient, userId: string) => {
+  const user = await client.users.cache.get(userId);
+  if (user) {
+    return user.username;
+  } else {
+    return "null";
+  }
+};
